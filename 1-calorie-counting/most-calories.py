73d5bbe9673c5270eb99e621d7159cf40d3bac6b
Anz Whitney <anwinynahte@gmail.com>
2023-05-11 17:48:13 -0400

    let's make this a class and optimize for multiple consecutive calls on the same list of elves, just because

--- 1-calorie-counting/most-calories.py
+++ 1-calorie-counting/most-calories.py
@@ -1,38 +1,43 @@
+import heapq
 import sys
+
 from collections import defaultdict
+
+class ElfInventories:
+
+    def __init__(self):
+        self._elves = defaultdict(list)
+        self._cal_totals = []
+
+    def parse_elves(self, f):
+        """Parse a list of elves' calorie inventories from an iterable f."""
+        elf_id = 0
+        for line in f:
+            # a blank line indicates a new elf's inventory
+            if line == "\n":
+                elf_id += 1
+            # consecutive non-blank lines are appended to the current elf's
+            # inventory
+            else:
+                self._elves[elf_id].append(int(line))
+
+    def print_top_n(self, n=3):
+        if not self._cal_totals:
+            for elf_id, inventory in self._elves.items():
+                heapq.heappush(self._cal_totals, (sum(inventory), elf_id))
+
+        # TODO: determine whether this usage of nlargest actually gets the
+        # benefits of the pre-heapification.
+        top_n = heapq.nlargest(n, self._cal_totals)
+        total = sum(cals for cals, _ in top_n)
+        print(f"The top {n} elves by calorie total are:")
+        for rank, elf in enumerate(top_n):
+            print(f"{rank}. Elf #{elf[1]} ({elf[0]} cals)")
+        print(f"Altogether, that's {total} calories.")
 
 def print_usage():
     print("Usage: python most_calories.py input_filename")
     sys.exit(0)
-
-def parse_elves(f):
-    """Parse a list of elves' calorie inventories from an iterable f."""
-    elf_id = 0
-    elves = defaultdict(list)
-    for line in f:
-        # a blank line indicates a new elf's inventory
-        if line == "\n":
-            elf_id += 1
-        # consecutive non-blank lines are appended to the current elf's
-        # inventory
-        else:
-            elves[elf_id].append(int(line))
-
-    return elves
-
-def most_calories(elves):
-    """Determine the index and calorie total of the elf with the most calories.
-    """
-    max_cals = 0
-    max_elf = None
-
-    for elf_id, inventory in elves.items():
-        cals = sum(inventory)
-        if cals > max_cals:
-            max_cals = cals
-            max_elf = elf_id
-
-    return max_elf, max_cals
 
 if __name__ == "__main__":
     if sys.argv[1] == "help":
