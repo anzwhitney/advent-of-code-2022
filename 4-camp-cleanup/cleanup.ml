d55138a69ce8041c38f9eb112f6f652867d77727
Anz Whitney <anwinynahte@gmail.com>
2023-05-15 12:52:14 -0400

    Add command line interface functionality to day 4 part 1

--- 4-camp-cleanup/cleanup.ml
+++ 4-camp-cleanup/cleanup.ml
@@ -9,7 +9,7 @@ module Section : Interval.COMPARABLE with type t = int = struct
   type order = Less | Equal | Greater
 
   let compare (s1 : t) (s2 : t) : order =
-    if s1 < s2 then Less else if t1 > t2 then Greater else Equal
+    if s1 < s2 then Less else if s1 > s2 then Greater else Equal
 end
 
 module SI = Interval.MakeClosedInterval (Section)
@@ -19,16 +19,16 @@ module SI = Interval.MakeClosedInterval (Section)
 let string_to_intervals (s : string) : SI.interval * SI.interval =
   match String.split_on_char ',' s with
   | [] | _ :: [] | _ :: _ :: _ :: _ ->
-      raise (Invalid_argument "Must have exactly two intervals per line")
+    raise (Invalid_argument "Must have exactly two intervals per line")
   | [ first; second ] ->
-      let interval substr =
-        match String.split_on_char '-' substr with
-        | [] | _ :: [] | _ :: _ :: _ :: _ ->
-            raise
-              (Invalid_argument "Must have exactly two endpoints per interval")
-        | [ startpt; endpt ] -> SI.interval startpt endpt
-      in
-      (interval first, interval second)
+    let interval substr =
+      match String.split_on_char '-' substr with
+      | [] | _ :: [] | _ :: _ :: _ :: _ ->
+        raise
+          (Invalid_argument "Must have exactly two endpoints per interval")
+      | [ startpt; endpt ] -> SI.interval (int_of_string startpt) (int_of_string endpt)
+    in
+    (interval first, interval second)
 
 let total_contained (l : (SI.interval * SI.interval) list) : int =
   let count_contained acc (a, b) =
@@ -37,3 +37,20 @@ let total_contained (l : (SI.interval * SI.interval) list) : int =
     | Contains -> acc + 1
   in
   List.fold_left count_contained 0 l
+
+
+(* Run from command line *)
+let usage_msg = "cleanup <input_filename>"
+
+let input_filename = ref ""
+
+let anon_fun filename = input_filename := filename
+
+let speclist = []
+
+let () = Arg.parse speclist anon_fun usage_msg;
+  let lines = read_lines input_filename in
+  let total = total_contained (List.map string_to_intervals lines) in
+  print_string "There are ";
+  print_int total;
+  print_endline " assignment pairs where one range fully contains the other."
